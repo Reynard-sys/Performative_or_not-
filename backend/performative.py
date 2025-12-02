478ae41bdf21b0c556c732b3c76e40b321331cf1
Rey <reynard.john.rabanal@gmail.com>
2025-12-03 03:01:57 +0800

    changes on backend

--- backend/performative.py
+++ backend/performative.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # Allow Next.js frontend to access the API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://performative-or-not.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
